417af32f77c7381615bea2eeccac2b823fdc2d32
Luis Tieres <luistieres@gmail.com>
2026-07-19 15:09:11 -0300

    feat: corrigindo bug

--- src/types/ServiceOrder.ts
+++ src/types/ServiceOrder.ts
@@ -9,4 +9,9 @@ export interface ServiceOrder {
   created_at: string
 }
 
-export type CreateServiceOrderData = Omit<ServiceOrder, 'id' | 'created_at'>
+export interface CreateServiceOrderData {
+  clientId: number
+  device: string
+  issue: string
+  status: ServiceOrderStatus
+}
